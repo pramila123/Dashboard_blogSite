4b8b8681468013cec968d519b9e751063b709ee3
DipeshShresth <dipeshxtha129@gmail.com>
2021-09-28 16:05:48 +0545

    view edit delete button added

--- src/component/MainView/Category/CategoryDataTable.js
+++ src/component/MainView/Category/CategoryDataTable.js
@@ -13,6 +13,15 @@ import "datatables.net-dt/js/dataTables.dataTables";
 import "datatables.net-dt/css/jquery.dataTables.min.css";
 import $ from "jquery";
 
+//icons
+import { AiOutlineDelete } from "react-icons/ai";
+import { AiOutlineEdit } from "react-icons/ai";
+import { GiBlackBook } from "react-icons/gi";
+
+import Tooltip from "@material-ui/core/Tooltip";
+import Zoom from "@material-ui/core/Zoom";
+import { Link } from "react-router-dom";
+
 function createData(name, calories, fat, carbs, protein) {
   return { name, calories, fat, carbs, protein };
 }
@@ -25,16 +34,6 @@ const rows = [
   createData("Gingerbread", 356, 16.0, 49, 3.9),
 ];
 
-// const StyledTableRow = withStyles((theme) => ({
-//   root: {
-//     "&:nth-of-type(odd)": {
-//       backgroundColor: "white",
-//     },
-//     "&:nth-of-type(even)": {
-//       backgroundColor: "grey",
-//     },
-//   },
-// }))(TableRow);
 const useStyle = makeStyles((theme) => ({
   tableRow: {
     whiteSpace: "nowrap",
@@ -44,6 +43,38 @@ const useStyle = makeStyles((theme) => ({
   },
   stripNoColor: {
     backgroundColor: "white",
+  },
+  editIcon: {
+    fontSize: "1.2rem",
+    color: "#2653D4",
+    marginLeft: "0.8rem",
+    "&:hover": {
+      backgroundColor: "#84818A",
+      // padding: "0.1rem",
+      borderRadius: "50%",
+      color: "white",
+    },
+  },
+  deleteIcon: {
+    fontSize: "1.2rem",
+    marginLeft: "0.8rem",
+    color: "#EF3B4F",
+    "&:hover": {
+      backgroundColor: "#84818A",
+      // padding: "0.1rem",
+      borderRadius: "50%",
+      color: "white",
+    },
+  },
+  viewIcon: {
+    fontSize: "1.1rem",
+    color: "#496C83",
+    "&:hover": {
+      backgroundColor: "#84818A",
+      // padding: "0.1rem",
+      borderRadius: "50%",
+      color: "white",
+    },
   },
 }));
 
@@ -71,7 +102,7 @@ export default function CategoryDataTable() {
               Category
             </TableCell>
 
-            <TableCell className={classes.tableRow} align="center">
+            <TableCell className={classes.tableRow} align="right">
               Action
             </TableCell>
           </TableRow>
@@ -85,8 +116,6 @@ export default function CategoryDataTable() {
                   ? { backgroundColor: "#e0e0d1" }
                   : { backgroundColor: "white" }
               }
-              //   className={index % 2 ? classes.stripColor : classes.stripNoColor}
-              //   sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
             >
               <TableCell align="left" className={classes.tableRow}>
                 {index}
@@ -95,7 +124,23 @@ export default function CategoryDataTable() {
                 {row.name}
               </TableCell>
 
-              <TableCell align="center">{row.fat}</TableCell>
+              <TableCell align="right">
+                <Tooltip title="View" TransitionComponent={Zoom} arrow>
+                  <Link>
+                    <GiBlackBook className={classes.viewIcon} />
+                  </Link>
+                </Tooltip>
+                <Tooltip title="Edit" TransitionComponent={Zoom} arrow>
+                  <Link>
+                    <AiOutlineEdit className={classes.editIcon} />
+                  </Link>
+                </Tooltip>
+                <Tooltip title="Delete" TransitionComponent={Zoom} arrow>
+                  <Link>
+                    <AiOutlineDelete className={classes.deleteIcon} />
+                  </Link>
+                </Tooltip>
+              </TableCell>
             </TableRow>
           ))}
         </TableBody>
--- src/component/Sidebar/SidebarMenuData.js
+++ src/component/Sidebar/SidebarMenuData.js
@@ -1,17 +1,15 @@
-import { AiOutlineHome } from "react-icons/ai";
+import { FaHome } from "react-icons/fa";
 import { IoIosPeople } from "react-icons/io";
 import { MdContactPhone } from "react-icons/md";
 import { BsFillBriefcaseFill } from "react-icons/bs";
 import { IoGitNetworkSharp } from "react-icons/io5";
-import {FaNetworkWired} from "react-icons/fa"
+import { FaNetworkWired } from "react-icons/fa";
 
 export const menuItems = [
-    { name: 'Dashboard', to: '/', icon: <AiOutlineHome /> },
-    { name: 'Category', to: '/category', icon: <IoGitNetworkSharp /> },
-    { name: 'Sub Category', to: '/subcategory', icon: <FaNetworkWired /> },
-    { name: 'Products', to: '/products', icon: <BsFillBriefcaseFill /> },
-    { name: 'About Us', to: '/aboutus', icon: <IoIosPeople /> },
-    { name: 'Contacts', to: '/contacts', icon: <MdContactPhone /> },
-    
-
-  ];
+  { name: "Dashboard", to: "/", icon: <FaHome /> },
+  { name: "Category", to: "/category", icon: <IoGitNetworkSharp /> },
+  { name: "Sub Category", to: "/subcategory", icon: <FaNetworkWired /> },
+  { name: "Products", to: "/products", icon: <BsFillBriefcaseFill /> },
+  { name: "About Us", to: "/aboutus", icon: <IoIosPeople /> },
+  { name: "Contacts", to: "/contacts", icon: <MdContactPhone /> },
+];
