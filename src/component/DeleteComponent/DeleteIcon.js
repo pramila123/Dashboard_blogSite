ffda90f34703902e23392b6bbd6aed8cc072159e
DipeshShresth <dipeshxtha129@gmail.com>
2021-10-01 13:13:24 +0545

    confirmation dialog added

--- src/component/DeleteComponent/DeleteIcon.js
+++ src/component/DeleteComponent/DeleteIcon.js
@@ -1,15 +1,26 @@
 import React, { useState } from "react";
 import Tooltip from "@material-ui/core/Tooltip";
 import Zoom from "@material-ui/core/Zoom";
-import { Link } from "react-router-dom";
 import { AiOutlineDelete } from "react-icons/ai";
 import { makeStyles } from "@material-ui/core/styles";
-import SuccessAlert from "../SuccessAlertComponent/SuccessAlert";
 import Alert from "@mui/material/Alert";
-import Button from "@mui/material/Button";
-import Stack from "@mui/material/Stack";
-import Dialog from "@material-ui/core/Dialog";
+
 import Snackbar from "@material-ui/core/Snackbar";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Slide from "@mui/material/Slide";
+import Button from "@mui/material/Button";
+import styled from "styled-components";
+
+const StyledDialog = styled(Dialog)`
+  .MuiBackdrop-root {
+    background-color: transparent;
+    backdrop-filter: blur(1px);
+  }
+`;
 
 const useStyles = makeStyles((theme) => ({
   alert: {
@@ -34,11 +45,28 @@ const useStyle = makeStyles((theme) => ({
     backgroundColor: "red",
   },
 }));
+
+const Transition = React.forwardRef(function Transition(props, ref) {
+  return <Slide direction="up" ref={ref} {...props} />;
+});
+
 const DeleteIcon = (props) => {
   const classes = useStyle();
 
+  //ConfirmationDialog
+  const [openConfimationDialog, setOpenConfimationDialog] =
+    React.useState(false);
+
+  const handleClickOpenConfimationDialog = () => {
+    setOpenConfimationDialog(true);
+  };
+
+  const handleCloseConfimationDialog = () => {
+    setOpenConfimationDialog(false);
+  };
+
   //for alert dialog box after submitting.
-  const [opendialog, setOpenDialog] = useState(false);
+  const [openDialog, setOpenDialog] = useState(false);
   const handleClickOpen = () => {
     setOpenDialog(true);
   };
@@ -51,13 +79,43 @@ const DeleteIcon = (props) => {
       <Tooltip title="Delete" TransitionComponent={Zoom} arrow>
         <AiOutlineDelete
           className={classes.deleteIcon}
-          onClick={handleClickOpen}
+          onClick={handleClickOpenConfimationDialog}
+          // onClick={handleClickOpen}
         />
       </Tooltip>
+      {/* Confirmation Dialog */}
 
+      <StyledDialog
+        open={openConfimationDialog}
+        TransitionComponent={Transition}
+        keepMounted
+        onClose={handleCloseConfimationDialog}
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle>{"Are you sure you want to delete?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-slide-description">
+            Let Google help apps determine location. This means sending
+            anonymous location data to Google, even when no apps are running.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleCloseConfimationDialog}>Disagree</Button>
+          <Button
+            onClick={() => {
+              handleClickOpen();
+              handleCloseConfimationDialog();
+            }}
+          >
+            Agree
+          </Button>
+        </DialogActions>
+      </StyledDialog>
+
+      {/* Snack bar */}
       <Snackbar
-        open={opendialog}
-        // autoHideDuration={1500}
+        open={openDialog}
+        autoHideDuration={1500}
         onClose={handleCloseDialog}
       >
         <Alert
